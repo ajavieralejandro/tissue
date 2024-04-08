63e9fa0ac15e9fde51751ed92b9e7b2902a478af
ajavieralejandro <amorosijavier@gmail.com>
2024-04-08 13:35:42 -0300

    Cell changes

--- src/components/Canvas/Experience.jsx
+++ src/components/Canvas/Experience.jsx
@@ -10,7 +10,7 @@ export const Experience = () => {
   return (
     <>
         <Canvas camera={
-      { fov: 10,  position: [1, 2, 3] }
+      { fov: 10,  position: [9, 2, 3] }
     }>
         <Suspense fallback={null}>
 
--- src/components/Inti/inti2.jsx
+++ src/components/Inti/inti2.jsx
@@ -33,6 +33,7 @@ export default function Inti2({itemData}) {
   return (
 
 <div>
+
 <Grid
   container
   direction="row"
@@ -56,21 +57,23 @@ export default function Inti2({itemData}) {
             <p className=" text-sm">     <StyledText>Thank you very much for opening the doors to me @intiargentina.
 Fluorescence is widely used in microscopy and an important tool for observing the distribution of specific molecules. Most molecules in cells do not fluoresce. Therefore, they have to be labeled with fluorescent molecules called fluorochromes. Molecules of interest can be labeled directly, (eg, DNA with DAPI) or they can be immunolabeled with fluorochromes that are coupled to specific antibodies. Cell fixation is usually required for immunostaining.
 </StyledText></p>
-<StyledText className=" text-sm">
+<div className='h-screen w-full'>        <Experience />
+</div>
+            </Grid>
+            <Grid item xs={12} md={6}>
+            <IntiGallery />
+
+            <StyledText className=" text-sm">
 Fluorescence microscopy also allows for time-lapse imaging of living cells or tissues. Hence, the proteins of interest can be tagged with genetically encoded fluorescent molecules such as GFP (green fluorescent protein). Molecules of interest can also be labeled using reversibly binding synthetic dyes (eg fura-2) or genetically modified naturally occurring proteins (eg GFP derivatives).
 
 </StyledText>
-
-            </Grid>
-            <Grid item xs={12} md={6}>
-            <Experience />
-
 
 
             </Grid>  
                
        </Grid>
-       <IntiGallery />
+         
+
        </div>
 
     );
