671907607bf24100ccf9c9092933d394c9804aac
ajavieralejandro <amorosijavier@gmail.com>
2024-04-08 13:48:44 -0300

    Fixing mobile bug

--- src/components/Inti/inti2.jsx
+++ src/components/Inti/inti2.jsx
@@ -57,7 +57,7 @@ export default function Inti2({itemData}) {
             <p className=" text-sm">     <StyledText>Thank you very much for opening the doors to me @intiargentina.
 Fluorescence is widely used in microscopy and an important tool for observing the distribution of specific molecules. Most molecules in cells do not fluoresce. Therefore, they have to be labeled with fluorescent molecules called fluorochromes. Molecules of interest can be labeled directly, (eg, DNA with DAPI) or they can be immunolabeled with fluorochromes that are coupled to specific antibodies. Cell fixation is usually required for immunostaining.
 </StyledText></p>
-<div className='h-screen w-full'>        <Experience />
+<div className='md:h-screen md:w-full'>        <Experience />
 </div>
             </Grid>
             <Grid item xs={12} md={6}>
