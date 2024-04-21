6eecb00d204f923030166d117d19a9a57b163036
ajavieralejandro <amorosijavier@gmail.com>
2024-04-21 00:19:53 -0300

    Work in progress leggend

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,33 +7,20 @@ import Footer from '../components/Footer/footer';
 import Parallax from '../components/parallax/Parallax';
 import { Container } from '@mui/material';
 import Gallery from '../components/Gallery/gallery';
-import Grid from '@mui/material/Grid';
-import ContactForm from '../components/contactForm/contactFom';
-import Gallery2 from '../components/Gallery2/Gallery2';
 import About from '../components/About/about';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material';
-import STLViewer from 'stl-viewer'
-import News from '../components/News/news';
-import Exhibition from '../components/Exhibition/exhibition';
-import Card4 from '../components/Cards/card4';
-import SectionHeader from '../components/SectionHeader';
-import Card3 from '../components/Cards/card3';
-import Inti from '../components/Inti/inti';
-import Inti2 from '../components/Inti/inti2';
-import Exhibitions from '../components/Exhibitions/exhibitions';
-import Parallax2 from '../components/parallax/Parallax2';
-import Menu from '../components/Menu/menu';
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useRef } from "react";
 import CustomCursor from "../CustomCursor";
 import YoutubeVideo from '../components/YoutubeVideo/youtubeVideo';
 import Timeline from '../components/Timeline/timeline';
+import SectionHeader from '../components/SectionHeader';
+import Inti2 from '../components/Inti/inti2';
 import "../styles/home.scss";
 import '../styles/reset.css';
 import '../App.css';
-import { Canvas } from '@react-three/fiber';
 
 const url = "https://storage.googleapis.com/ucloud-v3/ccab50f18fb14c91ccca300a.stl";
 
@@ -98,7 +85,7 @@ const HomePage = () => {
 
      </div>
       ) : (
-        <div className=" scroll-smooth h-screen w-screen" >
+        <div className="scroll-smooth h-screen w-screen m-0" >
         
              <Navbar />
 
@@ -118,8 +105,8 @@ const HomePage = () => {
         <YoutubeVideo embedId="a0R8Rdao1Do" />
 
         <SectionHeader className="section-header" title="INTI | Argentina" />
+        <Inti2/>
 
-        <Inti2 />
         </Container>
 
         
--- src/components/SceneTheater/example.jsx
+++ src/components/SceneTheater/example.jsx
@@ -1,41 +1,45 @@
 import {Canvas, useFrame} from '@react-three/fiber'
-
+import { Link } from 'react-router-dom';
+import Navbar from '../Navbar/navbar';
 //Theatre Imports
 import { ScrollControls,useScroll,Scroll} from '@react-three/drei'
 import {getProject,val} from '@theatre/core';
 import demoProjectState from './demoScene.json'
 import Texture from './Texture';
+import { useNavigate } from 'react-router-dom';
 import {
   SheetProvider, editable as e,
   PerspectiveCamera, useCurrentSheet
 } from '@theatre/r3f';
 function App() {
-
+  let navigate = useNavigate();
   const sheet = getProject('Demo Project',{state:demoProjectState}).sheet('Demo Sheet')
   return (
     <div className='h-screen w-screen'>
-    <Canvas gl={{preserveDrawingBuffer:true,phisicallyCorrectLight:true}}>
 
+    <Canvas gl={{preserveDrawingBuffer:true,phisicallyCorrectLight:true}}>
       <ScrollControls pages={5}>
       <SheetProvider sheet={sheet}>
 
     
       <Scene />
       <Scroll html>
+
           <div className='bg-transparent'>
-          <section class="container bg-black mx-auto p-10 md:p-20 transform duration-500">
-        <article class="flex flex-wrap md:flex-nowrap shadow-lg mx-auto max-w-xl ">
-            <img class="w-full md:w-40 h-auto" src="https://weandthecolor.com/wp-content/uploads/2012/03/A-Way-Out-Illustration-by-Matheus-Lopes-4563464.jpg" alt="" />
-            <div class="p-10 my-auto">
-                <h1 class="text-2xl font-semibold text-gray-800">A Way Out</h1>
-                <p class="text-base text-gray-400 mt-2">
-                    Super creative and colorful illustrations by Matheus Lopes. Check out more of his amazing artworks in his portfolio.
+          <section className="container   mx-auto p-10 md:p-20 transform duration-500">
+
+        <article className="flex flex-wrap bg-black md:flex-nowrap shadow-lg mx-auto max-w-xl ">
+            <div className="p-10 my-auto">
+                <h1 className="text-2xl font-semibold text-white">Work in progress...</h1>
+                <p className="text-base text-gray-400 mt-2">
                 </p>
             </div>
         </article>
     </section>
           </div>
+          
       </Scroll>
+   
       </SheetProvider>
 
       </ScrollControls>
